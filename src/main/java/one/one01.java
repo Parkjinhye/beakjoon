b34f7bfac404983f37d52703a710546357f95c1c
jinhye <jinh2376@gmail.com>
2021-10-07 18:07:20 +0900

    1번

---
+++ src/main/java/one/one01.java
@@ -0,0 +1,8 @@
+package one;
+
+public class one01 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
---
+++ src/main/java/one/one02.java
@@ -0,0 +1,9 @@
+package one;
+
+public class one02 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+
+    }
+}
